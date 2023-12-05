3c63fbccc9d12f4302c7b45d6bf69994d492bf38
Tomasz Mosur <tomasz.mosur@sixdx.com>
2023-12-05 13:11:09 +0100

    Advent of Code - Day 5 -

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -1,5 +1,7 @@
 package net.mosur
 
+import scala.collection.parallel.CollectionConverters._
+
 
 case class Entry(destinationStart: Long, sourceStart: Long, range: Long)
 
@@ -9,30 +11,34 @@ object Day5 extends App {
     val input = lines.mkString("\n")
     val regex = """(\d+\s)+\n""".r
     val iterator = regex.findAllIn(input).map(_.trim).toList
-    val seedInput = part match
-      case Part.One => iterator.head.split(" ").map(_.toLong).toList
-      case Part.Two => iterator.head.split(" ").map(_.toLong).sliding(2, 2).toList
-        .zipWithIndex
-        .flatMap { (a,index) =>
-          println(s"Processing ${index}")
-          a.head until a.head + a.last }
-
     val mapList = iterator.tail.map(
       _.split("\n")
         .map(_.split(" ").map(_.toLong))
         .map { array => Entry(array.head, array.tail.head, array.last) }
         .toList
     )
-    val result = seedInput.zipWithIndex.map { (seed, index) =>
-      println(s"Processing ${index} of ${seedInput.size}")
-      mapList.foldLeft(seed) { (currentNumber, map) =>
-        map.find { entry =>
-          entry.sourceStart <= currentNumber && entry.sourceStart + entry.range > currentNumber
-        }.map { entry =>
-          currentNumber - entry.sourceStart + entry.destinationStart
-        } getOrElse currentNumber
-      }
-    }.min
+    val result = part match
+      case Part.One => iterator.head.split(" ").map(_.toLong).toList
+      case Part.Two => iterator.head.split(" ").map(_.toLong).sliding(2, 2).toList
+        .zipWithIndex
+        .par
+        .flatMap { (a, index) =>
+          println(s"Processing ${index}")
+          val result = (a.head until a.head + a.last)
+            .par
+            .map { seed =>
+              //println(s"2:Processing ${index} ")
+              mapList.foldLeft(seed) { (currentNumber, map) =>
+                map.find { entry =>
+                  entry.sourceStart <= currentNumber && entry.sourceStart + entry.range > currentNumber
+                }.map { entry =>
+                  currentNumber - entry.sourceStart + entry.destinationStart
+                } getOrElse currentNumber
+              }
+            }
+          println(s"Finished processing ${index}")
+          result
+        }.min
     println(s"$part Result is $result. Expected result is: $expected ")
   }
 
