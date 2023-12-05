5d7fa61466506db30a059be9d395a686fec97e9c
Tomasz Mosur <tomasz@mosur.net>
2023-12-05 15:26:19 +0100

    Day 5 - final

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -17,28 +17,31 @@ object Day5 extends App {
         .map { array => Entry(array.head, array.tail.head, array.last) }
         .toList
     )
-    val result = part match
-      case Part.One => iterator.head.split(" ").map(_.toLong).toList
-      case Part.Two => iterator.head.split(" ").map(_.toLong).sliding(2, 2).toList
-        .zipWithIndex
-        .par
-        .flatMap { (a, index) =>
-          println(s"Processing ${index}")
-          val result = (a.head until a.head + a.last)
-            .par
-            .map { seed =>
-              //println(s"2:Processing ${index} ")
-              mapList.foldLeft(seed) { (currentNumber, map) =>
-                map.find { entry =>
-                  entry.sourceStart <= currentNumber && entry.sourceStart + entry.range > currentNumber
-                }.map { entry =>
-                  currentNumber - entry.sourceStart + entry.destinationStart
-                } getOrElse currentNumber
-              }
+
+    val ranges: List[List[Long]] = part match
+      case Part.One =>
+        // singe seed as range seed + 1
+        iterator.head.split(" ").map(_.toLong).toList.map { seed => List(seed, 1) }
+      case Part.Two => iterator.head.split(" ").map(_.toLong).sliding(2, 2).map(_.toList).toList
+
+    val result = ranges
+      .zipWithIndex
+      .par //parallel processing
+      .foldRight(Long.MaxValue) { (rangeDefAndIndex, globalMin) =>
+        val (rangeDef, index) = rangeDefAndIndex
+        val rangeResult = (rangeDef.head until rangeDef.head + rangeDef.last)
+          .foldRight(Long.MaxValue) { (seed, rangeMin) =>
+            val oneSeedResult = mapList.foldLeft(seed) { (currentNumber, map) =>
+              map.find { entry =>
+                entry.sourceStart <= currentNumber && entry.sourceStart + entry.range > currentNumber
+              }.map { entry =>
+                currentNumber - entry.sourceStart + entry.destinationStart
+              } getOrElse currentNumber
             }
-          println(s"Finished processing ${index}")
-          result
-        }.min
+            if (rangeMin > oneSeedResult) oneSeedResult else rangeMin
+          }
+        if (globalMin > rangeResult) rangeResult else globalMin
+      }
     println(s"$part Result is $result. Expected result is: $expected ")
   }
 
@@ -48,6 +51,6 @@ object Day5 extends App {
     execute(readLines("day5.txt"), 227653707, Part.One)
     println("Part 2:")
     execute(readLines("day5-small.txt"), 46, Part.Two)
-    execute(readLines("day5.txt"), 13768818, Part.Two)
+    execute(readLines("day5.txt"), 78775051, Part.Two)
   }
 }
