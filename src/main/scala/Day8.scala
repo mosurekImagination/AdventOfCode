6d255f019d4d53a516e6ae53f06a2721c4bad8ec
Tomasz Mosur <tomasz@mosur.net>
2023-12-09 17:45:01 +0100

    Advent Of Code - Day 8 - 9 'while loop' refactor

--- src/main/scala/Day8.scala
+++ src/main/scala/Day8.scala
@@ -1,57 +1,37 @@
 package net.mosur
 
+import scala.annotation.tailrec
+
 object Day8 extends App {
   case class Node(name: String, left: String, right: String)
 
   val execute: (List[String], Long, Part) => Unit = { (lines, expected, part) =>
 
     val path = lines.head
-    val nodes = lines.drop(2).map { line =>
-      val s"$node = ($left, $right)" = line
-      node -> Node(node, left, right)
+    val nodes = lines.drop(2).map {
+      case s"$node = ($left, $right)" => node -> Node(node, left, right)
     }.toMap
 
-    val startNodes = nodes.filter((key, _) => key.endsWith("A")).values.toList
-    val finishNodes = nodes.filter((key, _) => key.endsWith("Z")).values.toList
-
-    val result = part match {
-      case Part.One =>
-        var counter = 0
-        val cyclicPath = LazyList.iterate(path)(_ => path).flatten.iterator
-        val startNode = nodes("AAA")
-        val finishNode = nodes("ZZZ")
-        var currentNode = startNode
-        while (currentNode != finishNode) {
-          counter = counter + 1
-          if (cyclicPath.next() == 'L')
-            currentNode = nodes(currentNode.left)
-          else
-            currentNode = nodes(currentNode.right)
-        }
-        counter
-      case Part.Two => startNodes.flatMap { startNode =>
-        val cyclicIndexes = LazyList.continually(path.indices).flatten.iterator
-        val cyclicPath = LazyList.iterate(path)(_ => path).flatten.iterator
-        var currentNode = startNode
-        var endStepList = List[(Node, Int)]()
-        var endCounters = List[Long]()
-        var counter = 0
-        var currentIndex = cyclicIndexes.next()
-        while ((currentNode != startNode || counter == 0) && !endStepList.contains((currentNode, currentIndex))) {
-          if (finishNodes.contains(currentNode)) {
-            endCounters = endCounters :+ counter
-            endStepList = endStepList :+ (currentNode, currentIndex)
-          }
-          if (cyclicPath.next() == 'L')
-            currentNode = nodes(currentNode.left)
-          else
-            currentNode = nodes(currentNode.right)
-          counter = counter + 1
-          currentIndex = cyclicIndexes.next()
-        }
-        endCounters
-      }.lcm
+    val (startNodes, finishNodes) = part match {
+      case Part.One => (nodes.filter((key, _) => key.endsWith("AAA")).values.toList, nodes.filter((key, _) => key.endsWith("ZZZ")).values.toList)
+      case Part.Two => (nodes.filter((key, _) => key.endsWith("A")).values.toList, nodes.filter((key, _) => key.endsWith("Z")).values.toList)
     }
+
+    @tailrec
+    def countStepUntil(currentNode: Node, finishNodes: List[Node], cyclicPath: Iterator[Char], count: Int = 0): Int = {
+      if (finishNodes.contains(currentNode)) count
+      else
+        val nextNode = if (cyclicPath.next() == 'L')
+          nodes(currentNode.left)
+        else
+          nodes(currentNode.right)
+        countStepUntil(nextNode, finishNodes, cyclicPath, count + 1)
+    }
+
+    val result = startNodes.map { startNode =>
+      val cyclicPath = LazyList.iterate(path)(_ => path).flatten.iterator
+      countStepUntil(startNode, finishNodes, cyclicPath, 0).toLong
+    }.lcm
     println(s"$part Result is $result. Expected result is: $expected ")
   }
   time {
--- src/main/scala/Day9.scala
+++ src/main/scala/Day9.scala
@@ -1,25 +1,30 @@
 package net.mosur
 
+import scala.annotation.tailrec
+
 object Day9 extends App {
   case class Node(name: String, left: String, right: String)
+
+  def extractSublines(currentLine: List[Int]): List[List[Int]] =
+    @tailrec
+    def extractSublines(currentLine: List[Int], extractedLines: List[List[Int]]): List[List[Int]] =
+      if (currentLine.forall(_ == 0))
+        extractedLines
+      else
+        val differencesSubline = currentLine.sliding(2, 1).map(pair => pair.last - pair.head).toList
+        extractSublines(differencesSubline, differencesSubline :: extractedLines)
+
+    extractSublines(currentLine, List(currentLine))
 
   val execute: (List[String], Long, Part) => Unit = { (lines, expected, part) =>
     val result = lines.map(
       _.split(" ").map(_.toInt).toList
-    ).foldLeft(0) { (sum, line) =>
-      var extractedLines = List(line)
-      var currentLine = line
-      while (!currentLine.forall(_ == 0)) {
-        val differencesSubLine = currentLine.sliding(2, 1).map(pair => pair.last - pair.head).toList
-        extractedLines = extractedLines.appended(differencesSubLine)
-        currentLine = differencesSubLine
-      }
-      sum + extractedLines.foldRight(0)((subLine, extrapolatedSum) =>
-        part match
-          case Part.One => subLine.last + extrapolatedSum
-          case Part.Two => subLine.head - extrapolatedSum
-      )
-    }
+    ).map(extractSublines(_).foldRight(0)((subLine, extrapolatedSum) =>
+      part match
+        case Part.One => subLine.last + extrapolatedSum
+        case Part.Two => subLine.head - extrapolatedSum
+    )
+    ).sum
     println(s"$part Result is $result. Expected result is: $expected ")
 
   }
