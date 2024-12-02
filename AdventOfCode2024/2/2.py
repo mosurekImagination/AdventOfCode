0dd0d60ec4e7ac43f1613905b30489bbec680faa
Tomasz Mosur <tomasz@mosur.net>
2024-12-02 11:47:55 +0100

    DAY 2 - basic solution

--- AdventOfCode2024/2/2.py
+++ AdventOfCode2024/2/2.py
@@ -1,22 +1,37 @@
-import re
-from collections import Counter
-
 ### SOLVE FUNCTION IMPORT
 import sys
 import os
+
 # Add the grandparent directory to sys.path (two levels up)
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 from solve import solve
+
 ### SOLVE FUNCTION IMPORT
 
-
 def part1(file):
-    return 0
+    count = 0
+    for line in file:
+        line = [*map(int, line.split())]
+        r1 = all(1 <= line[i] - line[i-1] <= 3 for i in range(1, len(line)))
+        r2 = all(1 <= line[i-1] - line[i] <= 3 for i in range(1, len(line)))
+        if r1 or r2:
+            count = count + 1
+    return count
+
+
 def part2(file):
-    return 0
+    count = 0
+    for line in file:
+        line = [*map(int, line.split())]
+        variations = {tuple(line[:i] + line[i + 1:]) for i in range(len(line))}  # Use tuple to store variations
+        if any(
+                all(1 <= variation[i] - variation[i - 1] <= 3 for i in range(1, len(variation))) or
+                all(1 <= variation[i - 1] - variation[i] <= 3 for i in range(1, len(variation)))
+                for variation in variations
+        ):
+            count += 1  # Increment count if any variation matches either r1 or r2
+    return count
 
 
-solve("test.txt", 0, part1)
-solve("input.txt", 0, part1)
-solve("test.txt", 0, part2)
-solve("input.txt", 0, part2)
+solve(2, 236, part1)
+solve(4, 308, part2)
--- AdventOfCode2024/solve.py
+++ AdventOfCode2024/solve.py
@@ -1,9 +1,11 @@
 def solveFun(filename, expected_result, fun):
     with open(filename, 'r') as file:
         result = fun(file)
-    msg = f"Expected result is {expected_result} and calculated is: {result}"
-    if expected_result != result:
-        raise ValueError(msg)
+    if expected_result is None:
+        print(f"Result is {result}")
+    elif expected_result != result:
+            msg = f"Expected result is {expected_result} and calculated is: {result}"
+            raise ValueError(msg)
 
 def solve(test_result, final_result, *funcs):
     for func in funcs:
